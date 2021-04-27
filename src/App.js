22f2a93e6f2275164fbf0835f8cf2c1821be46dd
Alejandro <alejandroaperez1994g@gmail.com>
2021-04-27 16:10:39 -0400

    added To do form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
+import Todo from "./Todo";
 
 function App() {
+  const [todos, setTodos] = useState(["Primera Nota", "Segunda Nota"]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, input]);
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Notes To Do 🚀!! </h1>
+      <form>
+        <FormControl>
+          <InputLabel>Añadir Nota 👹 </InputLabel>
+          <Input
+            id="standard-basic"
+            label="Añadir Nota"
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+        </FormControl>
+
+        <Button
+          disabled={!input}
+          variant="contained"
+          type="submit"
+          color="primary"
+          onClick={addTodo}
         >
-          Learn React
-        </a>
-      </header>
+          Añadir
+        </Button>
+      </form>
+
+      <ul>
+        {todos.map((todo) => (
+          <Todo text={todo} />
+          // <li>{`${todo} 👹`}</li>
+        ))}
+      </ul>
     </div>
   );
 }
